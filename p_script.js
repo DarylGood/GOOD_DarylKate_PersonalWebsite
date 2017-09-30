7d52f2dbe87fbc50c9e1fb217e1eecffb0bcc6c8
Daryl Kate Good <darylkategood@gmail.com>
2017-09-30 14:00:30 +0800

    changed number of links in welcome page

---
+++ p_script.js
@@ -0,0 +1,24 @@
+function myFunction1() {
+    var x = document.getElementById("Demo1");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("Demo2");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("Demo3");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
